21b2df6a8ecc08d33a95d61e379b09adbb72d9de
dnova13 <dnova13@gmail.com>
2021-07-23 08:57:49 +0900

    Export Route

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-from flask import Flask, render_template, request,redirect
+from flask import Flask, render_template, request, redirect
 from scarpper import get_jobs
+import json
 
 # 서버 구축
 app = Flask("SuperScrapper")
 
-## fake db 생성
+# fake db 생성
 db = {}
 
 
@@ -12,7 +13,7 @@ db = {}
 @app.route("/")
 def home():
     return render_template("home.html")
-    ### 플라스크 기능 주 한 render_template에서 
+    # 플라스크 기능 주 한 render_template에서
     # 반드시 templates 폴더일경우 그 안에 있는 HTML 파일 들고옴.
 
 
@@ -22,35 +23,79 @@ def user(username):
     return f"your name is {username}"
 
 
-### 검색 기능 query argument
+# 검색 기능 query argument
 @app.route("/report")
 def report():
-    word = request.args.get("word") # word 쿼리의 값 받아옴.
+    word = request.args.get("word")  # word 쿼리의 값 받아옴.
     data = 111
-    
-    if word :
-        word = word.lower() ## 소문자로 변환
+
+    if word:
+        word = word.lower()  # 소문자로 변환
         existingJob = db.get(word)
-        
+
         # existingJob 값이 존재하면 패스
-        if existingJob :
+        if existingJob:
             jobs = existingJob
-        else : # none 일 경우 db 리스트에 저장
+        else:  # none 일 경우 db 리스트에 저장
             jobs = get_jobs(word)
             # print(jobs)
             db[word] = jobs
-        
-    else :
-        return redirect("/") ## 홈으로 리다이렉트
+
+    else:
+        return redirect("/")  # 홈으로 리다이렉트
 
     # return f"you are looking for a job in \"{word}\""
     return render_template("report.html",
-    searchingBy=word, 
-    data=data,
-    resultNumber=len(jobs),
-    jobs=jobs
-    )
+                           searchingBy=word,
+                           data=data,
+                           resultNumber=len(jobs),
+                           jobs=jobs
+                           )
 
+
+@app.route("/export")
+def export():
+
+    # try catch 문
+    try:
+        word = request.args.get('word')
+
+        # print(word)
+        if not word:
+            raise Exception()  # raise = throw 라고 보면됨.
+
+        word = word.lower()
+        jobs = db.get(word)
+
+        if not jobs :
+            raise Exception()
+        return f"Generate CSV for {word}"
+
+    except:
+        return redirect("/")
+
+
+# json test
+@app.route("/api/test1")
+def apiTest1():
+
+    _dic = {
+        "name": 1,
+        "feel": 2
+    }
+
+    return json.dumps(_dic)
+
+
+@app.route("/api/test2")
+def apiTest2():
+
+    _dic = {
+        "name": 1,
+        "feel": 2
+    }
+
+    return _dic
 
 
 # 로컬일 경우 안먹힐 수 도 있음 "0.0.0.0"
