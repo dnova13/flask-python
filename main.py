87932c7150dd094ebb19b55e01404283aa0589df
dnova13 <dnova13@gmail.com>
2021-07-23 05:09:45 +0900

    Dynamic URLs and Template

--- main.py
+++ main.py
@@ -1,20 +1,24 @@
-from flask import Flask
+from flask import Flask, render_template
 
-### 서버 구축
+# 서버 구축
 app = Flask("SuperScrapper")
 
 
-### @ : decorate 이고 이거 덕에 / 등으로 로 접속하는 라우트 만듬
+# @ : decorate 이고 이거 덕에 / 등으로 로 접속하는 라우트 만듬
 @app.route("/")
-def home() :
-    return "Hello! welcome"
+def home():
+    return render_template("home.html")
+    ### 플라스크 기능 주 한 render_template에서 
+    # 반드시 templates 폴더일경우 그 안에 있는 HTML 파일 들고옴.
 
-@app.route("/contact")
-def conn() :
-    return "contact!"
 
-### 로컬일 경우 안먹힐 수 도 있음 "0.0.0.0"
+# <username> /:username  username = 1 에 따라 동적으로 웹 제어 가능.
+@app.route("/<username>")
+def conn(username):
+    return f"your name is {username}"
+
+
+# 로컬일 경우 안먹힐 수 도 있음 "0.0.0.0"
 # app.run(host="0.0.0.0")
-
-### 로컬일 경우
+# 로컬일 경우
 app.run()
